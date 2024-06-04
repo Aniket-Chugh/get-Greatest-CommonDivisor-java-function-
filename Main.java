ac8eb5235ac82ad4fa9d85a08b512836ca6c95e3
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2024-06-05 00:45:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number : ");
+        int num2 = sc.nextInt();
+        int result = gcd(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is " + result);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
